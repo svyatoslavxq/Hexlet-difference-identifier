b4aad74a6460e0f9fb298832c176d7afb32f5d3d
svyatoslavxq <svyatoslavxq@yandex.ru>
2022-08-30 14:44:30 +0300

    fix: fix indexxx

--- src/index.js
+++ src/index.js
@@ -13,11 +13,11 @@ const readFile = (filePath) => {
   return fileData;
 };
 
-const getFileFormat = (fileName) => path.extname(fileName).slice(1);
+const fileFormat = (fileName) => path.extname(fileName).slice(1);
 
-const genDiff = (firstFilePath, secondFilePath, formatName) => {
-  const firstFileFormat = getFileFormat(firstFilePath);
-  const secondFileFormat = getFileFormat(secondFilePath);
+const genDiff = (firstFilePath, secondFilePath, formatName = 'stylish') => {
+  const firstFileFormat = fileFormat(firstFilePath);
+  const secondFileFormat = fileFormat(secondFilePath);
 
   const firstFileContent = readFile(firstFilePath);
   const secondFileContent = readFile(secondFilePath);
