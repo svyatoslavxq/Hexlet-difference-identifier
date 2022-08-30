7d3f71d783e33706a6287bc8ca0efd5b4f6744e3
svyatoslavxq <svyatoslavxq@yandex.ru>
2022-08-30 13:58:16 +0300

    fix: fix index

--- src/formatters/index.js
+++ src/formatters/index.js
@@ -2,8 +2,8 @@ import stylish from './stylish.js';
 import plain from './plain.js';
 import json from './json.js';
 
-const formatTree = (innerTree, formatName) => {
-  switch (formatName) {
+const formatTree = (innerTree, outputFormat) => {
+  switch (outputFormat) {
     case 'stylish': {
       return stylish(innerTree);
     }
@@ -14,7 +14,7 @@ const formatTree = (innerTree, formatName) => {
       return json(innerTree);
     }
     default:
-      throw new Error(`${formatName} is wrong format.`);
+      throw new Error(`${outputFormat} is wrong format.`);
   }
 };
 
